8e949122e3acb021994fa9e66a30933d04936d5f
inventivetalent <mail@inventivetalent.org>
2019-03-31 12:50:34 +0200

    add coordinate helpers

---
+++ src/main/java/org/inventivetalent/minetile/CoordinateConverter.java
@@ -0,0 +1,13 @@
+package org.inventivetalent.minetile;
+
+public class CoordinateConverter {
+
+	public static double localToGlobal(double d, int tileI, int tileSize, double centerD) {
+		return d - centerD + (tileI * tileSize * 2 * 16);
+	}
+
+	public static double globalToLocal(double d, int tileI, int tileSize, double centerD) {
+		return d - (tileI * tileSize * 2 * 16) + centerD;
+	}
+
+}
